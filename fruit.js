746ed0ea9a31caf626fca527ae89f3d6e9c4d25b
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-06-24 16:04:26 +1000

    fixed something relating to checking for a player

--- fruit.js
+++ fruit.js
@@ -1,24 +1,24 @@
 
 function fruit(board) {
     this.colour = "#ff0000";
-    this.position = {x: rand(1,30,1), y: rand(1,30,1)};
-    
+    this.position = { x: rand(1, 30, 1), y: rand(1, 30, 1) };
+
     function rand(min, max, interval) {
         if (typeof (interval) === 'undefined') interval = 1;
         var r = Math.floor(Math.random() * (max - min + interval) / interval);
         return r * interval + min;
     }
 
-    //while (board[this.position.x,this.position.y] == 1) {
-    //    this.position = {x: rand(1,30,1), y: rand(1,30,1)};
-    //}
-
-    this.draw = function (ctx) {
-        console.log(this.position);
-        ctx.fillStyle = this.colour;
-        ctx.fillRect((this.position.x * 20) - 20, (this.position.y * 20) - 20, 20, 20);
-
-
+    if (board != null) { 
+    while (board[this.position.x, this.position.y] == 1) {
+        this.position = { x: rand(1, 30, 1), y: rand(1, 30, 1) };
     }
+}
+
+this.draw = function (ctx) {
+    console.log(this.position);
+    ctx.fillStyle = this.colour;
+    ctx.fillRect((this.position.x * 20) - 20, (this.position.y * 20) - 20, 20, 20);
+}
 
 };
